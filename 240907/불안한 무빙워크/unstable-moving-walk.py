fd3c562a430c518af429a8f1282a5b3ca918cb84
Youngwoo <86396354+0right0705@users.noreply.github.com>
2024-09-07 18:00:56 +0900

    Title: 불안한 무빙워크, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240907/불안한 무빙워크/unstable-moving-walk.py
+++ 240907/불안한 무빙워크/unstable-moving-walk.py
@@ -5,7 +5,11 @@ for i in range(len(mw)):
     mw[i][0] = safety[i]
 zk = 0
 
-def Test1():
+def rotate():
+    global zk
+    global k
+
+    #회전
     lastM = mw[n * 2 - 1][0]
     lastP = mw[n * 2 - 1][1]
     for i in range(len(mw) - 1, 0, -1):
@@ -14,48 +18,65 @@ def Test1():
     mw[0][0] = lastM
     mw[0][1] = lastP
     mw[n - 1][1] = 0
-    
-    if mw[n-1][0] == 0:
-        return 0
 
-def Test2():
-    mw[0][0] -= 1
-    mw[0][1] = 1
-    for i in range(n - 1, 1, -1):
-        if mw[i][0] != 0 and mw[i][1] == 0:
-            mw[i][1] = 1
-            mw[i - 1][1] = 0
-            mw[i  - 1][0] -= 1
-    Test1()
-    if mw[n-1][0] == 0:
-        return 0
+def Test1():
+    global zk
+    global k
+    rotate()
 
-def Test3():
+    #0번 칸에 사람 올리기
     if mw[0][1] == 0 and mw[0][0] != 0:
         mw[0][1] = 1
-    Test1()
-    if mw[n-1][0] == 0:
-        return 0
-
-
-
-
-
-
-
-while zk < k:
-    if Test1() == 0:
+        mw[0][0] -=1
+    
+    if mw[n - 1][0] == 0:
         zk += 1
         if zk == k:
             print(1)
-            break
-    if Test2() == 0:
+
+def Test2():
+    global zk
+    global k
+
+    rotate()
+
+    
+    #앞으로 가기
+    for i in range(n - 1, 1, -1):
+        if mw[i][0] != 0 and mw[i][1] == 0:
+            mw[i][1] = 1
+            mw[i][0] -=1
+            mw[i - 1][1] = 0
+
+
+    if mw[n - 1][0] == 0:
         zk += 1
         if zk == k:
             print(2)
-            break
-    if Test3() == 0:
+    
+
+def Test3():
+    global zk
+    global k
+    rotate()
+    
+    #사람 올리기
+    if mw[0][1] == 0 and mw[0][0] != 0:
+        mw[0][1] = 1
+
+
+    if mw[n][0] == 0:
         zk += 1
         if zk == k:
             print(3)
-            break
+        return 0
+    
+
+
+while zk < k:
+    Test1()
+    print("1: ", mw)
+    Test2()
+    print("2: ", mw)
+    Test3()
+    print("3: ", mw)

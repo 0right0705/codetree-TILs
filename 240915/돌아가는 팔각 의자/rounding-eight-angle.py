67fa9e443b76d54b50b18d233acc313d9d2ecfee
Youngwoo <86396354+0right0705@users.noreply.github.com>
2024-09-15 18:27:43 +0900

    Title: 돌아가는 팔각 의자, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240915/돌아가는 팔각 의자/rounding-eight-angle.py
+++ 240915/돌아가는 팔각 의자/rounding-eight-angle.py
@@ -52,8 +52,8 @@ for z in range(tc):
 
 
     if n == 0:
-        if checkLeft(n+1) == True or checkRight(n-1):
-            rt.append([n, k])
+        # if checkLeft(n+1) == True or checkRight(n-1):
+        #     rt.append([n, k])
         if checkRight(0) == True:
             rt.append([1, -k])
             if checkRight(n+1) == True:
@@ -62,8 +62,8 @@ for z in range(tc):
                     rt.append([n+3, -k])
 
     if n == 4:
-        if checkLeft(n+1) == True or checkRight(n-1):
-            rt.append([n, k])
+        # if checkLeft(n+1) == True or checkRight(n-1):
+        #     rt.append([n, k])
         if checkRight(n) == True:
             rt.append([n-1, -k])
             if checkRight(n+1) == True:
@@ -71,23 +71,24 @@ for z in range(tc):
                 if checkRight(n+2) == True:
                     rt.append([n-3, -k])
     if n == 1:
-        if checkLeft(n+1) == True or checkRight(n-1):
-            rt.append([n, k])
-            if checkLeft(n) == True:
-                rt.append([n-1,-k])
+        # if checkLeft(n+1) == True or checkRight(n-1):
+        #     rt.append([n, k])
+        if checkLeft(n) == True:
+            rt.append([n-1,-k])
         if checkRight(n) == True:
             rt.append([n+1,-k])
             if checkRight(n+1) == True:
                 rt.append([n+2,k])
     if n == 2:
-        if checkLeft(n+1) == True or checkRight(n-1):
-            rt.append([n, k])
-            if checkLeft(n) == True:
-                rt.append([n-1,-k])
-                if checkLeft(n-1) == True:
-                    rt.append([n-2, k])
+        # if checkLeft(n+1) == True or checkRight(n-1):
+        #     rt.append([n, k])
+        if checkLeft(n) == True:
+            rt.append([n-1,-k])
+            if checkLeft(n-1) == True:
+                rt.append([n-2, k])
         if checkRight(n) == True:
             rt.append([n+1,-k])
+    rotate(n, k)
     for rotation in rt:
         rotate(rotation[0], rotation[1])
 
